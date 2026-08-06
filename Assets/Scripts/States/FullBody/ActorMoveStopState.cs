2a0e17c41710c39cc019000425ac9a63508e9b12
马陈哲 <3384373673@qq.com>
2026-08-06 17:09:11 +0800

    优化状态之间的细节，下一步加重力，加跳跃，接着是翻越

--- Assets/Scripts/States/FullBody/ActorMoveStopState.cs
+++ Assets/Scripts/States/FullBody/ActorMoveStopState.cs
@@ -2,14 +2,15 @@ using UnityEngine;
 
 public class ActorMoveStopState : ActorBaseState
 {
+    private const float ResumeLoopThreshold=0.8f;
+
     public ActorMoveStopState(Actor actor) : base(actor)
     {
     }
     private RootMotionData data;
     public override void Enter()
     {
-        LocomotionTransition transitions=
-            actor.runTimeData.blackboard.LastMoveState==LocomotionStateType.Jog
+        LocomotionTransition transitions=actor.runTimeData.blackboard.LastMoveState==LocomotionStateType.Jog
                 ?actor.animancerData.Jog
                 :actor.animancerData.Walk;
 
@@ -25,6 +26,23 @@ public class ActorMoveStopState : ActorBaseState
         }
         stateMachine.SetOnEndCallback(OnEndCallback);
     }
+
+    public override void ServerTick()
+    {
+        if(!actor.runTimeData.WantMove)return;
+        //期间移动切到loop
+        //如果播放进度大于规定值，切到start
+        if(NormalizedTime<ResumeLoopThreshold)
+        {
+            stateMachine.ChangeState(
+                stateRegistry.GetState<ActorMoveLoopState>());
+            return;
+        }
+
+        stateMachine.ChangeState(
+            stateRegistry.GetState<ActorMoveStartState>());
+    }
+
     private void OnEndCallback()
     {
         if(stateMachine.CurrentState!=this)return;
